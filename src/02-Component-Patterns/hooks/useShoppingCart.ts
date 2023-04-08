033012152059ddfd2e5657bef44e6306cb661368
Nazareno Sotto <nazareno.e.sotto@gmail.com>
2023-04-08 14:29:15 -0300

    control props alt

--- src/02-Component-Patterns/hooks/useShoppingCart.ts
+++ src/02-Component-Patterns/hooks/useShoppingCart.ts
@@ -14,28 +14,12 @@ export const useShoppingCart = () => {
     product: Product;
   }) => {
     setShoppingCart((oldState: any) => {
-      const productInCart: ProductInCart = oldState[product.id] || {
-        ...product,
-        count: 0,
-      };
-
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return {
-          ...oldState,
-          [product.id]: productInCart,
-        };
-      } else {
+      if (count === 0) {
         const { [product.id]: _toDelete, ...rest } = oldState;
         return rest;
       }
 
-      // if (count === 0) {
-      //   const { [product.id]: _toDelete, ...rest } = oldState;
-      //   return rest;
-      // }
-
-      // return { ...oldState, [product.id]: { ...product, count } };
+      return { ...oldState, [product.id]: { ...product, count } };
     });
   };
 
